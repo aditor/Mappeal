aebcc167d093ab5d1b3bb9fdc8f88cea638690e0
Adi <aditya.24@hotmail.com>
2018-06-03 18:44:07 -0300

    changing request layout

--- app/routes.py
+++ app/routes.py
@@ -13,44 +13,59 @@ def index():
     user = {'username': 'Miguel'}
     return render_template('index.html', title='Home', user=user)
 
-@app.route('/search')
+@app.route('/api/search')
 def search():
-    print("this is the city" + request.args.get('city', default = "",type = str))
-    zID = 'X1-ZWz1ggc591y5fv_6h6e1'
-    address = urllib.quote("Las Vegas Blvd") 
-    # request.args.get('fname', default = "",type = str)
-    city = request.args.get('city', default = "",type = str)
-    # https://en.wikipedia.org/wiki/ISO_3166-1
-    overpass_url = "http://overpass-api.de/api/interpreter"
-    overpass_query = """
-    [out:json];
-    area[name = "New York"];
-    (
-        way["building"~"residential|housing|terrace|detached|apartments|hotel"](area);
-    );
-    out center;
-    """
-    response = requests.get(overpass_url, 
-                            params={'data': overpass_query})
-    data = response.json()["elements"]
-    places = []
-    for loc in data:
-    	di = {}
-    	di['center'] = loc['center']
-    	di['type'] = loc['tags']['building']
-    	try:
-           di['addr'] = loc['tags']['addr:street']
-           di['name'] = loc['tags']['name']
-        except KeyError:
-           pass
+    output = json.dumps(request.json['stuff'])
+    print("this is the request" + output)
+    # zID = 'X1-ZWz1ggc591y5fv_6h6e1'
+    # address = urllib.quote("Las Vegas Blvd") 
+    # # request.args.get('fname', default = "",type = str)
+    # city = request.args.get('city', default = "",type = str)
+    # # https://en.wikipedia.org/wiki/ISO_3166-1
+    # overpass_url = "http://overpass-api.de/api/interpreter"
+    # overpass_query = """
+    # [out:json];
+    # area[name = "New York"];
+    # (
+    #     way["building"~"residential|housing|terrace|detached|apartments|hotel"](area);
+    # );
+    # out center;
+    # """
+    # response = requests.get(overpass_url, 
+    #                         params={'data': overpass_query})
+    # data = response.json()["elements"]
+    # places = []
+    # for loc in data:
+    # 	di = {}
+    # 	di['center'] = loc['center']
+    # 	di['type'] = loc['tags']['building']
+    # 	try:
+    #        di['addr'] = loc['tags']['addr:street']
+    #        di['name'] = loc['tags']['name']
+    #     except KeyError:
+    #        pass
 
-        places.append(di)
+    #     places.append(di)
 
-    finalDict = {}
-    finalDict['payload'] = places
+    # finalDict = {}
+    # finalDict['payload'] = places
 
-    json.dumps(finalDict, ensure_ascii=True)
-    # print(str(places))
+    # toPass = json.dumps(finalDict)
+
+
+    # user = {'username': 'TEST!'}
+
+    # return render_template('index.html', placeArr=toPass)
+    return {'test':"SANDWICH"};
+
+
+def pp_json(json_thing, sort=True, indents=4):
+    if type(json_thing) is str:
+        print(json.dumps(json.loads(json_thing), sort_keys=sort, indent=indents))
+    else:
+        print(json.dumps(json_thing, sort_keys=sort, indent=indents))
+    return None
+
     # with open('data2.txt', 'w') as outfile:
     #     json.dump(data, outfile)
 
@@ -68,15 +83,3 @@ def search():
     #     rank = country.find('rank').text
     #     name = country.get('name')
     #     print(name, rank)
-
-    user = {'username': 'TEST!'}
-    return render_template('index.html', title='TEST', placeArr=finalDict)
-
-
-def pp_json(json_thing, sort=True, indents=4):
-    if type(json_thing) is str:
-        print(json.dumps(json.loads(json_thing), sort_keys=sort, indent=indents))
-    else:
-        print(json.dumps(json_thing, sort_keys=sort, indent=indents))
-    return None
-
