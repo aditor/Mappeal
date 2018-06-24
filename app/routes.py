588c3197cd3ec448b1d611ad8f6300583c9407ae
Adi <aditya.24@hotmail.com>
2018-06-24 13:53:40 -0300

    clarifai working

--- app/routes.py
+++ app/routes.py
@@ -2,12 +2,15 @@ from flask import render_template
 from flask import request
 from app import app
 from collections import defaultdict
+from clarifai.rest import ClarifaiApp
+from clarifai.rest import Image as ClImage
+import numpy as np
 import requests
 import urllib
 import xml.etree.ElementTree as ET
 import json
 import random
-
+import urllib
 
 
 @app.route('/')
@@ -16,28 +19,87 @@ def index():
     user = {'username': 'Miguel'}
     return render_template('index.html', title='Home', user=user)
 
+@app.route('/analyze', methods=["POST"])
+def analyze():
+    app = ClarifaiApp(api_key="c306175f36704228af8ccef5332045dd")
+    model = app.models.get('general-v1.3')
+
+    allPoints = request.json['pleasework']
+    print(allPoints)
+
+    image = ClImage(url='https://samples.clarifai.com/metro-north.jpg')
+    aiRes = model.predict([image])
+    # print(aiRes)
+    return "doneeeee"
+
+
 @app.route('/api/search', methods=["POST"])
 def search():
     cityToSearch = request.json['city']
-
-    city_borders = requests.get("https://api.teleport.org/api/urban_areas/slug%3A" + cityToSearch + "/").json()['bounding_box']['latlon']
+    # print(test)
+    cityBorders = requests.get("https://api.teleport.org/api/urban_areas/slug%3A" + cityToSearch + "/").json()['bounding_box']['latlon']
 
     print("this is the request")
   
-    places_to_visit = [generate_random_points(city_borders) for i in xrange(100)]
-
+    placesToVisit = [generateRandomPoints(cityBorders) for i in xrange(100)]
+    analysis = streetViewAnalyze(placesToVisit)
     # snapToRoadStr = map(toLatLonPair, places_to_visit)
     # snapRequest = '|'.join(map(str, snapToRoadStr)) 
     # print(snapRequest)
     # changedPoints = requests.get("https://roads.googleapis.com/v1/snapToRoads?path=" +snapRequest+ "&key=AIzaSyAeFC4kvVAYZHn0xPeQzcFMg7F_wFO7wA4").json()
     # //print(places_to_visit)
-    return json.dumps(places_to_visit)
+    
+# RETURN BOTH THE ARRAY AND ALSO THE ANALYZED RESULTS IN A SINGLE JSON OBJECT
+# {arr: [], analyzed: []}
+    return json.dumps(placesToVisit)
 
 
-def generate_random_points(city_borders):
-    latitude = random.uniform(city_borders['south'], city_borders['north'])
-    longitude = random.uniform(city_borders['west'], city_borders['east'])
-    return latitude, longitude
+def generateRandomPoints(cityBorders):
+
+    midLon = (cityBorders['west'] + cityBorders['east']) / 2
+    midLat = (cityBorders['south'] + cityBorders['north']) / 2
+
+    gaussianLat = np.random.normal(midLat, 0.05)
+    gaussianLon = np.random.normal(midLon, 0.05)
+
+    strLatLon = str(gaussianLat) + "," + str(gaussianLon)
+
+    payload = {'key': 'AIzaSyAeFC4kvVAYZHn0xPeQzcFMg7F_wFO7wA4', 'latlng': strLatLon}
+    latLonPair = requests.get("https://maps.googleapis.com/maps/api/geocode/json", params=payload).json()['results'][0]['geometry']['location']
+
+    snappedLat = latLonPair['lat']
+    snappedLon = latLonPair['lng']
+
+    # print(str(snappedLat) + "," + str(snappedLon))
+
+    # return gaussianLat, gaussianLon
+    return snappedLat, snappedLon
+
+def streetViewAnalyze(pointsArr):
+    app = ClarifaiApp(api_key="c306175f36704228af8ccef5332045dd")
+    model = app.models.get('general-v1.3')
+    imageList=[]
+
+    for lat,lon in pointsArr:
+        params = { 'heading' : "151.78", 
+        'size' : "600x400",
+        'pitch' : "-0.76",
+        'location' : str(lat) + ","+ str(lon)}
+        encoded = urllib.urlencode(params)
+        streetViewURL = "https://maps.googleapis.com/maps/api/streetview?" + encoded
+        imageList.append(ClImage(url=streetViewURL))
+    
+    print(streetViewURL)
+    B, C = split_list(imageList)
+    aiRes = model.predict(B)
+    
+    print(aiRes)
+
+
+def split_list(a_list):
+    half = len(a_list)/2
+    return a_list[:half], a_list[half:]
+
 
 
 def toLatLonPair(x):
